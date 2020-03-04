faf2ced92f07b0292e4e82b510202cd7432c036a
Adam Espino <aespino1987@gmail.com>
2020-03-03 18:07:57 -0600

    change from router to app.get in my html routes

--- connection.js
+++ connection.js
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/FitnessTracker"
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/daRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
--- routes/daRoutes.js
+++ routes/daRoutes.js
@@ -1,16 +1,16 @@
-const router = require("express").router();
+const app = require("express");
 const path = require("path");
 
-router.get("/", function(req,res){
+app.get("/", function(req,res){
     res.sendFile(path.join(__dirname,"../public/index.html"));
 });
 
-router.get("/exercise", (req,res) => {
+app.get("/exercise", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-router.get("/stats", (req,res) => {
+app.get("/stats", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
-module.exports = router;
+module.exports = app;
